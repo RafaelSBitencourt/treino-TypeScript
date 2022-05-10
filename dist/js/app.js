09ec5f52645cefc488937f6e7d22384309186e1e
Epica1995*_ <rafaelsbiten@hotmail.com>
2022-05-10 20:26:40 -0300

    alterando arquivos js por arquivos Typescript

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,3 @@
 import { Negociacao } from "./models/negociacao.js";
-
-var teste1 = new Negociacao('02/03/2012','15','10.00');
-console.log(teste1)
+var teste1 = new Negociacao('02/03/2012', '15', '10.00');
+console.log(teste1);
