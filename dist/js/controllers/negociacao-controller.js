fa419fd3e1088a930dd2a778915535d1bb043ed2
Epica1995*_ <rafaelsbiten@hotmail.com>
2022-05-12 21:20:11 -0300

    add-controllers-and-typing-for-all

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,3 +1,4 @@
+import { Negociacao } from "../models/negociacao.js";
 export class NegociacaoController {
     inputData;
     inputQuantidade;
@@ -8,8 +9,20 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValor);
+        const negociacao = this.criaNegociacao();
+        this.clearFormulario();
+        console.log(negociacao);
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    clearFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
     }
 }
